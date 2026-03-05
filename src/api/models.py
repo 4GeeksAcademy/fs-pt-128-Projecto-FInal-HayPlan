3089c250b10826a91a644712aee091ab68ebb8ec
jcasib <jcasib01@gmail.com>
2026-03-05 16:33:56 +0000

    Retoques en modelos

--- src/api/models.py
+++ src/api/models.py
@@ -5,23 +5,24 @@ import enum
 from datetime import datetime, timezone
 from typing import List
 from flask_bcrypt import generate_password_hash, check_password_hash
+import secrets
 
 db = SQLAlchemy()
 
-# class PlanStatus(enum.Enum):
-#     PROPUESTA = "propuesta"
-#     VOTACION = "votacion"
-#     EN_CURSO = "en_curso"
-#     CERRADO = "cerrado"
+class PlanStatus(enum.Enum):
+    PROPUESTA = "propuesta"
+    VOTACION = "votacion"
+    EN_CURSO = "en_curso"
+    CERRADO = "cerrado"
 
-# # PF
-# # tabla auxiliar compuesta por PK-> user.id & PK -> group.id 
-# # Relacion muchos a muchos
-# group_members = db.Table(
-#     "group_members",
-#     db.Column("user_id", db.Integer, db.ForeignKey("user.id"), primary_key=True),
-#     db.Column("group_id", db.Integer, db.ForeignKey("group.id"), primary_key=True)
-# )
+# PF
+# tabla auxiliar compuesta por PK-> user.id & PK -> group.id 
+# Relacion muchos a muchos
+group_members = db.Table(
+    "group_members",
+    db.Column("user_id", db.Integer, db.ForeignKey("user.id"), primary_key=True),
+    db.Column("group_id", db.Integer, db.ForeignKey("group.id"), primary_key=True)
+)
     
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
@@ -30,11 +31,11 @@ class User(db.Model):
     username: Mapped[str] = mapped_column(String(120), unique=True, nullable=True)
     is_active: Mapped[bool] = mapped_column(Boolean(), default=True, nullable=False)
 
-    # #PF
-    # admin_groups: Mapped[List["Group"]] = relationship("Group", back_populates="admin")
-    # groups: Mapped[List["Group"]] = relationship("Group", secondary=group_members, back_populates="members")
-    # plans_organizer: Mapped[List["Plan"]] = relationship("Plan", back_populates="organizer") #esta relacion nos permite completar estadisticas de un usuario relacionado a planes. 
-    # # --PF
+    #PF
+    admin_groups: Mapped[List["Group"]] = relationship("Group", back_populates="admin")
+    groups: Mapped[List["Group"]] = relationship("Group", secondary=group_members, back_populates="members")
+    plans_organizer: Mapped[List["Plan"]] = relationship("Plan", back_populates="organizer") #esta relacion nos permite completar estadisticas de un usuario relacionado a planes. 
+    # --PF
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password).decode('utf-8')
@@ -51,134 +52,146 @@ class User(db.Model):
         }
 
 
-# class Plan(db.Model):
-#     id: Mapped[int] = mapped_column(primary_key=True)
-#     title: Mapped[str] = mapped_column(String(120), nullable=False)
-#     description: Mapped[str] = mapped_column(Text, default="")
-#     group_id: Mapped[int] = mapped_column(ForeignKey("group.id"), nullable=False)
-#     organizer_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
-#     status: Mapped[PlanStatus] = mapped_column(Enum(PlanStatus), default=PlanStatus.PROPUESTA)
-#     location: Mapped[str] = mapped_column(String(120), default="")
-#     date: Mapped[datetime] = mapped_column(DateTime, nullable=False)
-#     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
+class Plan(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(120), nullable=False)
+    description: Mapped[str] = mapped_column(Text, default="")
+    group_id: Mapped[int] = mapped_column(ForeignKey("group.id"), nullable=False)
+    organizer_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+    status: Mapped[PlanStatus] = mapped_column(Enum(PlanStatus), default=PlanStatus.PROPUESTA)
+    location: Mapped[str] = mapped_column(String(120), default="")
+    date: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
 
-#     group: Mapped["Group"] = relationship("Group", back_populates="plans")
-#     organizer: Mapped["User"] = relationship("User", back_populates="plans_organizer")
-#     votes: Mapped[List["Vote"]] = relationship("Vote", back_populates="plan", cascade="all, delete-orphan")
-#     ratings: Mapped[List["PlanRating"]] = relationship("PlanRating", back_populates="plan", cascade="all, delete-orphan")
+    group: Mapped["Group"] = relationship("Group", back_populates="plans")
+    organizer: Mapped["User"] = relationship("User", back_populates="plans_organizer")
+    votes: Mapped[List["Vote"]] = relationship("Vote", back_populates="plan", cascade="all, delete-orphan")
+    memories: Mapped[List["PlanMemory"]] = relationship("PlanMemory", back_populates="plan", cascade="all, delete-orphan")
+    # Falta en Plan:
+    expenses: Mapped[List["Expense"]] = relationship("Expense", back_populates="plan", cascade="all, delete-orphan")
+    ratings: Mapped[List["PlanRating"]] = relationship("PlanRating", back_populates="plan", cascade="all, delete-orphan")
 
-#     def serialize(self):
-#         average_rating = None
-#         if self.ratings:
-#             average_rating = round(sum(rating.score for rating in self.ratings)/len(self.ratings), 1)
+    def serialize(self):
+        average_rating = None
+        if self.ratings:
+            average_rating = round(sum(rating.score for rating in self.ratings)/len(self.ratings), 1)
         
-#         return {
-#             "id": self.id,
-#             "title": self.title,
-#             "description": self.description,
-#             "group_id": self.group_id,
-#             "organizer_id": self.organizer_id,
-#             "organizer_username": self.organizer.username,
-#             "status": self.status.value,
-#             "location": self.location,
-#             "date": self.date.isoformat(),
-#             "created_at": self.created_at.isoformat(),
-#             "rating": average_rating,
-#             "rating_count": len(self.ratings)
-#         }
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "group_id": self.group_id,
+            "organizer_id": self.organizer_id,
+            "organizer_username": self.organizer.username,
+            "status": self.status.value,
+            "location": self.location,
+            "date": self.date.isoformat(),
+            "created_at": self.created_at.isoformat(),
+            "rating": average_rating,
+            "rating_count": len(self.ratings)
+        }
 
-# class Vote(db.Model):
-#     id: Mapped[int] = mapped_column(primary_key=True)
-#     plan_id: Mapped[int] = mapped_column(ForeignKey("plan.id"), nullable=False)
-#     user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
-#     vote: Mapped[bool] = mapped_column(Boolean, nullable=False)
+class Vote(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    plan_id: Mapped[int] = mapped_column(ForeignKey("plan.id"), nullable=False)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+    vote: Mapped[bool] = mapped_column(Boolean, nullable=False)
 
-#     plan: Mapped["Plan"] = relationship("Plan", back_populates="votes")
-#     voter: Mapped["User"] = relationship("User", back_populates="votes")
+    plan: Mapped["Plan"] = relationship("Plan", back_populates="votes")
+    voter: Mapped["User"] = relationship("User")
 
-#     def serialize(self):
-#         return {
-#             "id": self.id,
-#             "plan_id": self.plan_id,
-#             "user_id": self.user_id,
-#             "vote": self.vote
-#         }
+    def serialize(self):
+        return {
+            "id": self.id,
+            "plan_id": self.plan_id,
+            "user_id": self.user_id,
+            "vote": self.vote
+        }
     
-# class PlanRating(db.Model):
-#     id: Mapped[int] = mapped_column(primary_key=True)
-#     plan_id: Mapped[int] = mapped_column(ForeignKey("plan.id"), nullable=False)
-#     user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
-#     score: Mapped[int] = mapped_column(Integer, nullable=False)
+class PlanRating(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    plan_id: Mapped[int] = mapped_column(ForeignKey("plan.id"), nullable=False)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+    score: Mapped[int] = mapped_column(Integer, nullable=False)
 
-#     plan: Mapped["Plan"] = relationship("Plan", back_populates="ratings")
-#     user: Mapped["User"] = relationship("User", back_populates="ratings")
+    plan: Mapped["Plan"] = relationship("Plan", back_populates="ratings")
+    user: Mapped["User"] = relationship("User")
 
-#     def serialize(self):
-#         return{
-#             "id": self.id,
-#             "plan_id": self.plan_id,
-#             "user_id": self.user_id,
-#             "score": self.score
-#         }
+    def serialize(self):
+        return{
+            "id": self.id,
+            "plan_id": self.plan_id,
+            "user_id": self.user_id,
+            "score": self.score
+        }
     
-# class PlanMemory(db.Model):
-#     id: Mapped[int] = mapped_column(primary_key=True)
-#     plan_id: Mapped[int] = mapped_column(ForeignKey("plan.id"), nullable=False)
-#     user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
-#     comment: Mapped[str] = mapped_column(String(500), nullable=False)
-#     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
+class PlanMemory(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    plan_id: Mapped[int] = mapped_column(ForeignKey("plan.id"), nullable=False)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+    comment: Mapped[str] = mapped_column(String(500), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
 
-#     plan: Mapped["Plan"] = relationship("Plan", back_populates="memories")
-#     user: Mapped["User"] = relationship("User", back_populates="memories")
+    plan: Mapped["Plan"] = relationship("Plan", back_populates="memories")
+    user: Mapped["User"] = relationship("User")
 
-#     def serialize(self):
-#         return {
-#             "id": self.id,
-#             "plan_id": self.plan_id,
-#             "user_id": self.user_id,
-#             "username": self.user.username,
-#             "comment": self.comment,
-#             "created_at": self.created_at.isoformat()
-#         }
+    def serialize(self):
+        return {
+            "id": self.id,
+            "plan_id": self.plan_id,
+            "user_id": self.user_id,
+            "username": self.user.username,
+            "comment": self.comment,
+            "created_at": self.created_at.isoformat()
+        }
     
-# class Expense(db.Model):
-#     id: Mapped[int] = mapped_column(primary_key=True)
-#     plan_id: Mapped[int] = mapped_column(ForeignKey("plan.id"), nullable=False)
-#     paid_by_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
-#     description: Mapped[str] = mapped_column(String(500), nullable=False)
-#     total_amount: Mapped[float] = mapped_column(Float, nullable=False)
+class Expense(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    plan_id: Mapped[int] = mapped_column(ForeignKey("plan.id"), nullable=False)
+    paid_by_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+    description: Mapped[str] = mapped_column(String(500), nullable=False)
+    total_amount: Mapped[float] = mapped_column(Float, nullable=False)
 
-#     plan: Mapped["Plan"] = relationship("Plan", back_populates="expenses")
-#     paid_by: Mapped["User"] = relationship("User", back_populates="expenses")
+    plan: Mapped["Plan"] = relationship("Plan", back_populates="expenses")
+    paid_by: Mapped["User"] = relationship("User")
     
-#     def serialize(self):
-#         return {
-#             "id": self.id,
-#             "plan_id": self.plan_id,
-#             "paid_by_id": self.user_id,
-#             "paid_by": self.user.username,
-#             "description": self.description,
-#             "total_amount": self.total_amount
-#         }
+    def serialize(self):
+        return {
+            "id": self.id,
+            "plan_id": self.plan_id,
+            "paid_by_id": self.paid_by_id,
+            "paid_by": self.paid_by.username,
+            "description": self.description,
+            "total_amount": self.total_amount
+        }
 
-# # PF
+# PF
 
-# class Group(db.Model):
-#     id: Mapped[int] = mapped_column(primary_key=True)
-#     name: Mapped[str] = mapped_column(String(120), nullable=False)
-#     description: Mapped[str] = mapped_column(Text, default="", nullable=False)
-#     admin_id: Mapped[int]= mapped_column(ForeignKey("user.id"), nullable=False)
+def generate_invite_code():
+    code = secrets.token_hex(4).upper()
 
-#     #relaciones
-#     admin: Mapped["User"] = relationship("User", back_populates="admin_groups")
-#     members: Mapped[List["User"]] = relationship("User", secondary=group_members, back_populates="groups")
-#     plans: Mapped[List["Plan"]] = relationship("Plan", back_populates="group", cascade="all, delete-orphan")
+    while Group.query.filter_by(invite_code = code).first():
+        code = secrets.token_hex(4).upper()
+    return code
 
-#     def serialize(self):
-#         return{
-#             "id": self.id,
-#             "name": self.name,
-#             "description": self.description,
-#             "admin_id": self.admin_id
-#         }
-#     # --PF
+class Group(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), nullable=False)
+    description: Mapped[str] = mapped_column(Text, default="", nullable=False)
+    invite_code: Mapped[str] = mapped_column(String(8), unique=True, nullable=False, default=lambda: generate_invite_code())
+    admin_id: Mapped[int]= mapped_column(ForeignKey("user.id"), nullable=False)
+
+    #relaciones
+    admin: Mapped["User"] = relationship("User", back_populates="admin_groups")
+    members: Mapped[List["User"]] = relationship("User", secondary=group_members, back_populates="groups")
+    plans: Mapped[List["Plan"]] = relationship("Plan", back_populates="group", cascade="all, delete-orphan")
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "invite_code": self.invite_code,
+            "admin_id": self.admin_id
+        }
+    # --PF
