3de5303f2df1644778640f0645f5cfbdba3687b8
Robb <rfao300@hotmail.com>
2026-03-10 14:26:14 +0000

    Implementacion de cards y pagina descubre

--- src/api/routes.py
+++ src/api/routes.py
@@ -512,24 +512,24 @@ def get_votes(group_id, plan_id):
 
 @api.route('/ticketmaster-events', methods=['GET'])
 def get_ticketmaster_events():
-    # API KEY desde las variables de entorno del backend
     api_key = os.getenv("TICKETMASTER_API_KEY")
     
     if not api_key:
-        return jsonify({"error": "API Key no configurada en el servidor"}), 500
+        return jsonify({"error": "API Key no configurada"}), 500
 
-    # URL de Ticketmaster para eventos
+    # CAPTURA la ciudad que viene del Frontend
+    city = request.args.get('city')
+    
     url = f"https://app.ticketmaster.com/discovery/v2/events.json?apikey={api_key}"
+    
+    if city:
+        url += f"&city={city}"
 
     try:
-        # Realizamos la petición externa a Ticketmaster
         response = requests.get(url)
         response.raise_for_status() 
-        
         data = response.json()        
-        
         return jsonify(data), 200
 
     except requests.exceptions.RequestException as e:
-        # Manejo de errores de conexión o de la API externa
         return jsonify({"error": "Error al conectar con Ticketmaster", "details": str(e)}), 502
