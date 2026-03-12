36d52b8df0c897e88a6ad18ed1f313d5f714a103
Pedro Franceschi <117317386+FranceschiPedro@users.noreply.github.com>
2026-03-12 22:53:54 +0000

    Landing page for presentation

--- src/front/pages/Hero.jsx
+++ src/front/pages/Hero.jsx
@@ -5,21 +5,38 @@ export const Hero = () => {
 
     return (
         <div className="container my-5">
-            <div className="p-5 text-center bg-body-tertiary rounded-3">
-                <img src={iconLogoColor} width="100" height="100" />
-                <h1 className="text-body-emphasis">Organiza tus salidas</h1>
-                <h2 className="text-body-emphasis">sin complicaciones!</h2>
+            <div className="hero-box p-5 text-center rounded-4 position-relative overflow-hidden">
+
+                {/* Background Logo */}
+                <img
+                    src={iconLogoColor}
+                    className="hero-logo"
+                    alt=""
+                />
+
+                <h1 className="text-white">Organiza tus salidas</h1>
+                <h2 className="text-warning">sin complicaciones!</h2>
+
                 <p className="col-lg-8 mx-auto fs-5 text-muted">
                     Hay Plan organiza ideas, votaciones y fechas en un solo lugar para que los planes realmente sucedan.
                 </p>
-                <div className="d-inline-flex gap-2 mb-5">
-                    <Link to="/signup" className="d-inline-flex align-items-center btn btn-primary btn-lg px-4 rounded-pill" type="button">
+
+                <div className="d-inline-flex gap-2 mb-4">
+                    <Link
+                        to="/signup"
+                        className="btn btn-warning btn-lg px-4 rounded-pill"
+                    >
                         Sign up
                     </Link>
-                    <Link to="/login" className="btn btn-outline-secondary btn-lg px-4 rounded-pill" type="Link">
+
+                    <Link
+                        to="/login"
+                        className="btn btn-outline-light btn-lg px-4 rounded-pill"
+                    >
                         Login
                     </Link>
                 </div>
+
             </div>
         </div>
     )
--- src/front/pages/LandingPage.jsx
+++ src/front/pages/LandingPage.jsx
@@ -9,13 +9,7 @@ export const LandingPage = () => {
     return (
          <div className="container py-5">
             <Hero />
-
             <HowItWorks />
-
-            <p className="lead mb-4">
-                HayPlan helps groups organize plans, vote on options, and keep everything in one place.
-            </p>
-
         </div>
     );
 }; 
